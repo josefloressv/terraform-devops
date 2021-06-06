9b4746fae1100af180852fc55a2eccab307eae70
Jose Flores <josefloressv@gmail.com>
2021-06-05 18:19:01 -0600

    Mutable infrastructure with packer - ubuntu. reorganizing..

--- aws/scripts/packages-to-install-ubuntu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Wait for cloud-init to finish
-# thanks https://dev.to/hsatac/building-a-ubuntu-virtualbox-image-on-aws-ec2-with-packer-m00
-while [ ! -f /var/lib/cloud/instance/boot-finished ]; do echo 'Waiting for cloud-init...'; sleep 1; done
-
-# installing example package
-apt update
-apt install -y ansible 2>/dev/null
