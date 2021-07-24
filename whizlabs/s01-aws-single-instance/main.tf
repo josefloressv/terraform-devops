21d835d235464df15bb830ad8bbcd6a0562d6457
José Flores <josefloressv@gmail.com>
2021-07-23 20:32:08 -0600

    Adding arguments like profile and region in provider, deploying two instances with the argument count in resource and added tags

--- whizlabs/s01-aws-single-instance/main.tf
+++ whizlabs/s01-aws-single-instance/main.tf
@@ -1,8 +1,18 @@
 provider "aws" {
   # last version
+
+  # AWS credentials profile stored in ~./aws/credentials
+  profile = "default"
+
+  # US East N. Virginia
+  region = "us-east-1"
 }
 
 resource "aws_instance" "whizlabs_ec2_demo" {
+  count         = 2
   ami           = "ami-0dc2d3e4c0f9ebd18" # Amazon Linux 2 AMI (HVM), SSD Volume Type (64-bit x86)
   instance_type = "t2.micro"
+  tags = {
+    Name = "Whizlabs-instance-${count.index}"
+  }
 }
