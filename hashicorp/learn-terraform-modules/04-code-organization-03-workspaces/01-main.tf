a03ad54d14694fb36e3f6b6333ba7bfa8f585cc7
José Flores <josefloressv@gmail.com>
2021-07-29 17:07:47 -0600

    Terraform infrastructure with workspace for different environment, the same code but different variables configurations according to the environment. Magic is done by Terraform workspaces

---
+++ hashicorp/learn-terraform-modules/04-code-organization-03-workspaces/01-main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "random_pet" "petname" {
+  length    = 4
+  separator = "-"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${var.prefix}-${random_pet.petname.id}"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${var.prefix}-${random_pet.petname.id}/*"
+            ]
+        }
+    ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_object" "webapp" {
+  acl          = "public-read"
+  key          = "index.html"
+  bucket       = aws_s3_bucket.bucket.id
+  content      = file("${path.module}/assets/index.html")
+  content_type = "text/html"
+
+}
+
+# Reference
+# https://learn.hashicorp.com/tutorials/terraform/organize-configuration?in=terraform/modules#separate-states
