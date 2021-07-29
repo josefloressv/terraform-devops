e5c32c3c08ee80d79f6e18cde49369fe537c2a7f
José Flores <josefloressv@gmail.com>
2021-07-29 12:45:11 -0600

    Separating configuration in two .tf files one for dev an other for prod

---
+++ hashicorp/learn-terraform-modules/04-code-organization/01-main-prod.tf
@@ -0,0 +1,69 @@
+/*terraform {
+  backend "remote" {
+    organization = "josefloressv-training"
+
+    workspaces {
+      name = "code-organization"
+    }
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+
+resource "random_pet" "petname" {
+  length    = 4
+  separator = "-"
+}
+*/
+
+resource "aws_s3_bucket" "prod" {
+  bucket = "${var.prod_prefix}-${random_pet.petname.id}"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${var.prod_prefix}-${random_pet.petname.id}/*"
+            ]
+        }
+    ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_object" "prod" {
+  acl          = "public-read"
+  key          = "index.html"
+  bucket       = aws_s3_bucket.prod.id
+  content      = file("${path.module}/assets/index.html")
+  content_type = "text/html"
+
+}
+
+# Reference
+# https://learn.hashicorp.com/tutorials/terraform/organize-configuration?in=terraform/modules
+# https://github.com/hashicorp/learn-terraform-code-organization
