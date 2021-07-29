71c6b55d179da2d3c0a4e3d17ab190dc79c12128
José Flores <josefloressv@gmail.com>
2021-07-29 16:18:52 -0600

    Terraform infrastructure multi-environment dev & prod in separated directory, but with duplicated code still.

---
+++ hashicorp/learn-terraform-modules/04-code-organization-02-directories/dev/01-main.tf
@@ -0,0 +1,67 @@
+terraform {
+  backend "remote" {
+    organization = "josefloressv-training"
+
+    workspaces {
+      name = "code-organization"
+    }
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "random_pet" "petname" {
+  length    = 3
+  separator = "-"
+}
+
+resource "aws_s3_bucket" "dev" {
+  bucket = "${var.dev_prefix}-${random_pet.petname.id}"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${var.dev_prefix}-${random_pet.petname.id}/*"
+            ]
+        }
+    ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_object" "dev" {
+  acl          = "public-read"
+  key          = "index.html"
+  bucket       = aws_s3_bucket.dev.id
+  content      = file("${path.module}/../assets/index.html")
+  content_type = "text/html"
+
+}
+
+# Reference
+# https://learn.hashicorp.com/tutorials/terraform/organize-configuration?in=terraform/modules
+# https://github.com/hashicorp/learn-terraform-code-organization
